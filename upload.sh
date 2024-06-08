959e4829066170181e69fd7caca68ee19373e688
Cory Duplantis <cld251@gmail.com>
2024-06-08 12:57:33 -0500

    Better upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
-while inotifywait -e modify,create ./learnwithwelcometo; do 
-  rsync -lvr ./learnwithwelcometo/ ./remote/learnwithwelcometo;
+GAME=$1
+
+if [[ "$1" == "" ]]; then
+  echo "USAGE: ./upload.sh <GAME_NAME>";
+  exit 1
+fi
+
+while inotifywait -r -e modify,create ./$GAME; do 
+  rsync -lvr --ignore-times --update ./$GAME/ ./remote/$GAME;
 done
+
+
+# inotifywait -m -e modify,create --format '%w%f' ./learnwithwelcometo |
+# while read FILE
+# do
+#   echo "File modified: $FILE"
+#   cp $FILE ./remote/learnwithwelcometo
+#   echo "DONE copying.."
+# done
